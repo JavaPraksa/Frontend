3ad3c6f3ef085d083ecbe703c2fc03be82299cfd
anagrahovac <grahovac.ana99@gmail.com>
2022-03-31 09:30:49 +0200

    feat: add available vehicles component

--- RentACar/src/app/app-routing.module.ts
+++ RentACar/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvailableCarsDisplayComponent } from './available-cars-display/available-cars-display.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'registration', component: RegistrationComponent }
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'available-cars', component: AvailableCarsDisplayComponent }
 ];
 
 @NgModule({
--- RentACar/src/app/app.module.ts
+++ RentACar/src/app/app.module.ts
@@ -10,23 +10,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AvailableCarsDisplayComponent } from './available-cars-display/available-cars-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    AvailableCarsDisplayComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +44,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatFormFieldModule,
     MatInputModule,
     MatToolbarModule,
-    MatSnackBarModule
+    MatSnackBarModule, 
+    MatIconModule,
+    MatSelectModule
 
   ],
   providers: [],
