5df96814da7722cbfe9ad0731455980f2f1c168e
dejandeki123 <dejanpetrusic123@gmail.com>
2022-04-04 12:08:06 +0200

    new

--- RentACar/src/app/service/user.service.ts
+++ RentACar/src/app/service/user.service.ts
@@ -1,6 +1,7 @@
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 //import { profile } from 'console';
 import { userServiceApi } from '../app.consts';
 import { LoginUser } from '../login/LoginUser';
@@ -20,7 +21,10 @@ export class UserService {
 
   register(user: User) {
     return this.http.post(userServiceApi + 'user/register', user)
-  }
+  }     
 
+  grabUser(): Observable<User> {
+    return this.http.get<User>('http://localhost:8082/user/' + sessionStorage.getItem('username'));
+  }
   
 }
